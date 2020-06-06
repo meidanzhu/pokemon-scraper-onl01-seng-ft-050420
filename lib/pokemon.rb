22b5076a014f20f10967fc3c0b4c51aeadc091fb
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-06-06 18:22:18 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -24,11 +24,9 @@ class Pokemon
       WHERE id = ?
     SQL
 
-    pokemon = db.execute(sql, id)
-    new_pokemon = self.new(pokemon)
-    new_pokemon.id = pokemon[0][0]
-    new_pokemon.name = pokemon[0][1]
-    new_pokemon.type = pokemon[0][2]
-    new_pokemon
+    pokemon = db.execute(sql, id).flatten
+    name = pokemon[0]
+    type = pokemon[1]
+    Pokemon.new(id: id, name: name, type: type, db: db)
    end
 end
