20d325664d9a2b45f52865dd474506b676a2d7a8
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-06-06 18:21:13 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,11 +11,11 @@ class Pokemon
 
   def self.save(name, type, db)
     sql = <<-SQL
-          INSERT INTO pokemon (name, type)
-          VALUES (?, ?)
-        SQL
+       INSERT INTO pokemon (name, type)
+       VALUES (?, ?)
+     SQL
 
-        db.execute(sql, name, type)
+     db.execute(sql, name, type)
   end
 
   def self.find(id, db)
